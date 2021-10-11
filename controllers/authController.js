cd21a2df5a9dcda8da37b4c37465fa9f057b203b
AgustinTafura <agustintafura@hotmail.com>
2021-10-11 02:28:12 -0300

    auth: register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,8 @@
 const { Op, Sequelize } = require("sequelize");
 const {User}  = require('../models/index');
+const sendEmail = require('../lib/sendgrid')
+const {generateAccessToken} = require('../lib/jwt')
+const helpers = require('../lib/helpers')
 
 
 module.exports = {
@@ -7,19 +10,40 @@ module.exports = {
   createUser:  async (req, res, next) => {
     const { email, password } = req.body
     
-    encryptPassword = await helpers.encryptPassword(password);
+    try {
+      const {  email, password } = req.body;
+  
+      // Validate
+      if (!(email && password)) {
+        res.status(400).send("email and password are required");
+      }
 
-    await User.create({email,password:encryptPassword})
-    .then(user=>{
-      const client = new SMTPClient({
-        user: 'user',
-        password: 'password',
-        host: 'smtp.your-email.com',
-        ssl: true,
-      });
-    })
-    .catch(err=>res.status(400).send(err))
-    // const newToken = generateAccessToken(user.id)
+      const oldUser = await User.findOne({ where:{email} });
+
+      if (oldUser) {
+        return res.status(409).send("User Already Exist. Please Login");
+      }
+
+      encryptPassword = await helpers.encryptPassword(password);
+
+      await User.create({
+        email: email.toLowerCase(),
+        password:encryptPassword}
+      )
+      .then(async user=>{
+        const token = await generateAccessToken(user.id)
+        user.dataValues.token = token
+        //sent welcome email 
+        sendEmail
+        console.log('user', user)
+        res.json(user);
+      })
+      .catch(err=>{console.log(111, err);res.status(400).send(err)})
+
+    } catch (err) {
+      console.log(444, err);
+    }
   
   }
 }
+
